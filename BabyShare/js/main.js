9b73f40b3ae72cf3aa3964ea98128c7a23704bfc
Jetrey Bond <jbond@agileoasis.com>
2017-04-28 14:40:44 -0400

    end of day commit

--- BabyShare/js/main.js
+++ BabyShare/js/main.js
@@ -1,8 +1,26 @@
+//first level slide downs
 $('.submitting p:first(), .checking p:first()').click(function() {
 	$(this).siblings('.inside').slideToggle();
-	$(this).parent().siblings().children('.inside').slideUp();
+	$(this).parent().siblings().children('.inside').slideUp().children().children('.inside').slideUp();
+
 });
+//second level slide downs
 $('.nap p:first(), .play p:first(), .eat p:first(), .diaper p:first()').click(function(){
 	$(this).siblings('.inside').slideToggle();
 	$(this).parent().siblings().children('.inside').slideUp();
-})
+});
+
+//save functions
+function saveNap() {
+	console.log('nap');
+}
+function savePlay() {
+	console.log('play');
+}
+function saveMeal() {
+	console.log('meal');
+}
+function saveDaiper() {
+	console.log('diaper');
+}
+
