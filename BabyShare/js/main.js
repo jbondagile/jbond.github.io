01c516aceab8908a17a91bafdea8c47b38b1cabd
jbond <jbond@agileoasis.com>
2017-05-04 08:38:44 -0400

    implemented checking

--- BabyShare/js/main.js
+++ BabyShare/js/main.js
@@ -63,6 +63,12 @@ var dataService = {
   },
 };
 dataService.init();
+window.onload = function() {
+  loadNap();
+  loadDiaper();
+  loadPlay();
+  loadMeal();
+}
 
 //first level slide downs
 $('.submitting p:first(), .checking p:first()').click(function() {
@@ -71,10 +77,15 @@ $('.submitting p:first(), .checking p:first()').click(function() {
 
 });
 //second level slide downs
-$('.nap p:first(), .play p:first(), .eat p:first(), .diaper p:first()').click(function(){
+$('.submitting .nap p:first(), .submitting .play p:first(), .submitting .eat p:first(), .submitting .diaper p:first()').click(function(){
 	$(this).siblings('.inside').slideToggle();
 	$(this).parent().siblings().children('.inside').slideUp();
 });
+$('.checking .nap p:first(), .checking .play p:first(), .checking .eat p:first(), .checking .diaper p:first()').click(function(){
+  $(this).siblings('.inside').slideToggle();
+  $(this).parent().siblings().children('.inside').slideUp();
+});
+
 
 //save functions
 function saveNap() {
@@ -107,8 +118,110 @@ function saveDiaper() {
 }
 
 function loadNap() {
+  dataService.getListNap().then(function(response){
+    // call a helper function to populate results in our UI
+    $.each(response.data, function(i, db){
+      // Find a <table> element with id="myTable":
+      var table = document.getElementById("napTable");
 
+      // Create an empty <tr> element:
+      var row = table.insertRow(1);
+
+      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+
+      // Add some text to the new cells:
+      cell1.innerHTML = db.startTime;
+      cell2.innerHTML = db.endTime;
+    })
+  })
 }
 
+function loadPlay() {
+  dataService.getListPlay().then(function(response){
+    // call a helper function to populate results in our UI
+    $.each(response.data, function(i, db){
+      // Find a <table> element with id="myTable":
+      var table = document.getElementById("playTable");
 
+      // Create an empty <tr> element:
+      var row = table.insertRow(1);
+
+      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      var cell1 = row.insertCell(0);
+
+      // Add some text to the new cells:
+      cell1.innerHTML = db.notes;
+    })
+  })
+}
+
+function loadMeal() {
+  dataService.getListMeal().then(function(response){
+    // call a helper function to populate results in our UI
+    $.each(response.data, function(i, db){
+      // Find a <table> element with id="myTable":
+      var table = document.getElementById("eatTable");
+
+      // Create an empty <tr> element:
+      var row = table.insertRow(1);
+
+      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+
+      // Add some text to the new cells:
+      cell1.innerHTML = db.food;
+      cell2.innerHTML = db.amount;
+    })
+  })
+}
+
+function loadDiaper() {
+  dataService.getListDiaper().then(function(response){
+    // call a helper function to populate results in our UI
+    $.each(response.data, function(i, db){
+      // Find a <table> element with id="myTable":
+      var table = document.getElementById("diaperTable");
+
+      // Create an empty <tr> element:
+      var row = table.insertRow(1);
+
+      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+
+      // Add some text to the new cells:
+      cell1.innerHTML = db.createdAt;
+      if(db.Dry == 'True') {
+        cell2.innerHTML = '&#10003;';
+      }
+      else {
+        cell2.innerHTML = '&#10005;';
+      }
+      if(db.wet == 'True') {
+        cell3.innerHTML = '&#10003;';
+      }
+      else {
+        cell3.innerHTML = '&#10005;';
+      }
+      if(db.soft == 'True') {
+        cell4.innerHTML = '&#10003;';
+      }
+      else {
+        cell4.innerHTML = '&#10005;';
+      }
+      if(db.normal == 'True') {
+        cell5.innerHTML = '&#10003;';
+      }
+      else {
+        cell5.innerHTML = '&#10005;';
+      }
+    })
+  })
+}
 
