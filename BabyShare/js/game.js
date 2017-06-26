393a8190dff8bf8bbe50fc879cbb83424775bff5
jbond <jbond@agileoasis.com>
2017-06-26 11:49:36 -0400

    sdAS

--- BabyShare/js/game.js
+++ BabyShare/js/game.js
@@ -1,23 +1,6 @@
 $(document).ready(function(){
 
-  //login
-  var x = document.cookie;
 
-  function readCookie(name) {
-      var nameEQ = name + "=";
-      var ca = document.cookie.split(';');
-      for(var i=0;i < ca.length;i++) {
-          var c = ca[i];
-          while (c.charAt(0)==' ') c = c.substring(1,c.length);
-          if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-      }
-      return null;
-  }
-
-  if(!(readCookie('login'))) {
-    window.location.href = "login.html";
-  }
-  else {
     //setting up database (hopefully)
     var dataService = {
       init: function(){
@@ -274,5 +257,5 @@ $(document).ready(function(){
         // table.deleteRow(i);
       }
     }
-  }
+
 })
